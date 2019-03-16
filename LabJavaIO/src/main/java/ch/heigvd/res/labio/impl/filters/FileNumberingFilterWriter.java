d88b835b4eb397bac1be717a430b963b738fe728
Mateo Tutic <mateo@tutic.ch>
2019-03-16 21:12:30 +0100

    FileNumberingFilterWriter implemented

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -11,13 +11,15 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int counter = 0;  // line counter
+  private char lastChar;    // last char written
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,17 +27,37 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; ++i) {
+      write(str.charAt(i));
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; ++i) {
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // Check for \r line separator
+    if (lastChar == '\r' && c != '\n'){
+      out.write(++counter + "\t");
+    }
+
+    // Check if it's first line
+    if (counter == 0) {
+      out.write(++counter  + "\t");
+    }
+    out.write(c);
+
+    // Check for \n line separator
+    if (c == '\n') {
+      out.write(++counter + "\t");
+    }
+
+    lastChar = (char) c;
   }
 
 }
